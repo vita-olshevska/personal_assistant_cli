6f2ccf7f5e3fe4eacd5a9101e6e88904d4f23051
Vita Olshevska <vaolshevska@gmail.com>
2021-08-27 15:25:33 +0300

    [Task-0001] Implement basic version of 'analyze' method  in context_analyzer module

--- core/context_analyzer/context_analyzer.py
+++ core/context_analyzer/context_analyzer.py
@@ -1,3 +1,7 @@
+from core.address_book.address_book import AddressBook
+from core.note_book.note_book import NoteBook
+from core.sort_manager.sort_manager import SortManager
+
 import types
 
 
@@ -9,5 +13,44 @@ class ContextAnalyzer:
             - хто робить цю команду (AddressBook чи NoteBook).
             - команду
             - все інше, що після імені
-    """
-        pass
+        """
+        responsible_module, command, user_data = None, None, None
+        words = request.split()
+        if request == "off":
+            responsible_module = "main"
+            command = "off"
+        elif "sort" == words[0]:
+            responsible_module = SortManager
+            command = SortManager.sort
+            user_data = " ".join(words[1:])
+        elif "record" == words[1]:
+            responsible_module = AddressBook
+            if "add" == words[0]:
+                command = AddressBook.add
+            elif "change" == words[0]:
+                command = AddressBook.change
+            elif "delete" == words[0]:
+                command = AddressBook.delete
+            elif "filter" == words[0]:
+                command = AddressBook.filter
+            elif "show birthday" in request:
+                # TODO: think about such case
+                command = AddressBook.show_users_birthday
+
+            user_data = " ".join(words[2:])
+        elif "note" == words[1]:
+            responsible_module = NoteBook
+            if "add" == words[0]:
+                command = NoteBook.add
+            elif "change" == words[0]:
+                command = NoteBook.change
+            elif "delete" == words[0]:
+                command = NoteBook.delete
+            elif "filter" == words[0]:
+                command = NoteBook.filter_for_tags
+            elif "tag" == words[0]:
+                command = NoteBook.add_tag_to_note
+
+            user_data = " ".join(words[2:])
+
+        return responsible_module, command, user_data
