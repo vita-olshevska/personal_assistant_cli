57dbc40a37b6a1662801590963ee99c2d8aef723
Dmytro <one.sage888@gmail.com>
2021-08-29 15:39:21 +0300

    Tasks 01 02 04 05 06 07 08 09 10

--- core/common/db_config.py
+++ core/common/db_config.py
@@ -14,21 +14,6 @@ conn.commit()
 cur.execute("""CREATE TABLE IF NOT EXISTS notes(
    id INTEGER PRIMARY KEY AUTOINCREMENT,
    tag TEXT,
-   note TEXT,
-   name TEXT);
+   note TEXT);
 """)
 conn.commit()
-
-# cur.execute("""INSERT INTO contacts(id, name, phone, email, birthday)
-#   VALUES('00001', 'Test', '380671232233', 'next@email.com', '1988-09-07');""")
-# conn.commit()
-#
-# cur.execute("""INSERT INTO notes(id, tag, note, name)
-#   VALUES('00001', '#first', 'some text', 'Alex');""")
-# conn.commit()
-
-# cur.execute(
-#     """SELECT * FROM contacts;""")
-# all_results = cur.fetchall()
-# for i in all_results:
-#     print(i, end='\n')
