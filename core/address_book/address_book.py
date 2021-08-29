3631863537fb6776a692d560abdd5b3dabb62abe
Dmytro <one.sage888@gmail.com>
2021-08-29 15:39:21 +0300

    Tasks 01 02 04 05 06 07 08 09 10

--- core/address_book/address_book.py
+++ core/address_book/address_book.py
@@ -2,45 +2,47 @@ import core.common.db_config as db
 
 
 class AddressBook:
-    def add(self, name, phone: int, address, email, birthday):
+    def add(self, arg):
         """
         Створює новий запис в адресну книжку за вказаним іменем.
         :param request: dict - стрічка, де спочатку обов'язково йде ім'я, а потім одна або декілька інформацій в будь-якому порядку: адреса, номер тлф, email, день народження
         :return: str - виводить (повертає) стрічку з повідомленням користувачу, де каже, що все добре і все добавлено, або вказує, що є помилка і яка.
         """
-
-        add_record = (name, phone, address, email, birthday)
+        fields = list(arg.keys())
+        add_record = (arg[fields[0]], arg[fields[1]])
         try:
-            db.cur.execute("""INSERT INTO contacts(name, phone, address, email, birthday)
-        VALUES(?, ?, ?, ?, ?);""", add_record)
+            db.cur.execute(f"""INSERT INTO contacts(name, {fields[1]})
+        VALUES(?, ?);""", add_record)
             db.conn.commit()
         except db.sqlite3.Error as error:
             print("Something went wrong", error)
+        return 'Record added'
 
-    def change(self, name, value, field) -> str:
+    def change(self, arg) -> str:
         """
         Змінює запис за вказаним іменем в адресній книзі.
         :param request: dict - стрічка, де спочатку обов'язково йде ім'я, а потім нова інформація (адреса, номер тлф, email чи день народження)
         :return: str - повертає повідомлення користувачу, де каже, що все добре і змінено, або вказує, що є помилка і яка.
         """
-        update_note = (value, name)
-        if field == 'name':
+        fields = list(arg.keys())
+        update_note = (arg[fields[1]], arg[fields[0]])
+        if fields[1] == 'name':
             sql = """UPDATE contacts
               SET name = ?
               WHERE name = ?"""
-        elif field == 'phone':
+        elif fields[1] == 'phone':
             sql = """UPDATE contacts
               SET phone = ?
               WHERE name = ?"""
-        elif field == 'address':
+        elif fields[1] == 'address':
             sql = """UPDATE contacts
               SET address = ?
               WHERE name = ?"""
-        elif field == 'email':
+        elif fields[1] == 'email':
             sql = """UPDATE contacts
               SET email = ?
               WHERE name = ?"""
-        elif field == 'birthday':
+        elif fields[1] == 'birthday':
             sql = """UPDATE contacts
               SET birthday = ?
               WHERE name = ?"""
@@ -49,19 +51,22 @@ class AddressBook:
             db.conn.commit()
         except db.sqlite3.Error as error:
             print("Something went wrong", error)
+        return f'{fields[1]} changed'
 
-    def delete(self, name: str):
+    def delete(self, arg):
         """
         Видаляє запис за вказаним іменем у адресній книзі. Акщо вхідна стрічка містить тільки ім'я, то видаляється все, що збережено за цим іменем.
         :param request: dict - стрічка, де спочатку обов'язково ім'я, а потім або інформація, яку треба видалити (адреса, номер тлф, email чи день народження)
         :return: str - повертає повідомлення користувачу, де каже, що все добре і видалено, або вказує, що є помилка і яка.
         """
+
         try:
             db.cur.execute("""DELETE FROM contacts
-              WHERE name = ?""", (name,))
+              WHERE name = ?""", (arg['name'],))
             db.conn.commit()
         except db.sqlite3.Error as error:
             print("Something went wrong", error)
+        return f'Record {arg["name"]} deleted'
 
     def filter(self, request: str):
         """
@@ -70,23 +75,46 @@ class AddressBook:
         :return: str - повертає стрічку, де записана вся інформаційна лінія (імя, email, тлф, день народження), в якій було співпадіння. Якщо таких ліній декілька, то вони всі
         в стрічці розділені знаком \n. Якщо співпадіння немає, або помилка, то повертає стрічку-повідомлення про це.
         """
+        result = ''
         try:
             db.cur.execute(
                 """SELECT * FROM contacts WHERE name like ? OR phone like ? OR email like ? OR birthday like ?;""", ('%'+request+'%', '%'+request+'%', '%'+request+'%', '%'+request+'%'))
             for i in db.cur.fetchall():
-                print(i, end='\n')
+                result += str(i) + '\n'
         except db.sqlite3.Error as error:
             print("Something went wrong", error)
+        if result == '':
+            return 'No matches'
+        else:
+            return result
 
     def show_users_birthday(self, interval: int):
         """
         Знаходить користувачів, у яких день народження через задану кількість днів від поточної дати.: param days_number: - кількість днів, що додається до поточної дати.: return: - повертаємо стрічку з записом всіх імен користувачів ті їх днів народження, наприклад "ім'я: yyyy-mm-dd, \n ім'я: yyyy-mm-dd, \n...".
         """
-
+        result = ''
         try:
             db.cur.execute(
                 f"""SELECT name FROM contacts WHERE strftime('%j', birthday) = strftime('%j', (date('now','+{interval} days'))) ;""")
             for i in db.cur.fetchall():
-                print(i[0], end='\n')
+                result += str(i[0]) + '\n'
         except db.sqlite3.Error as error:
             print("Something went wrong", error)
+        if result == '':
+            return 'No birthdays in this day'
+        else:
+            return result
+
+    def get_records(self, arg):
+        result = ''
+        try:
+            db.cur.execute(
+                f"""SELECT * FROM contacts ;""")
+            for i in db.cur.fetchall():
+                result += str(i) + '\n'
+        except db.sqlite3.Error as error:
+            print("Something went wrong", error)
+        if result == '':
+            return 'Address book is empty yet'
+        else:
+            return result
--- core/note_book/note_book.py
+++ core/note_book/note_book.py
@@ -3,42 +3,56 @@ import core.common.db_config as db
 
 class NoteBook:
 
-    def add(self, note: str, name: str):
-        add_note = (note, name)
+    def add(self, arg):
+        value = (arg['text'],)
+
         try:
-            db.cur.execute("""INSERT INTO notes(note, name)
-            VALUES(?, ?);""", add_note)
+            db.cur.execute("""INSERT INTO notes(note)
+            VALUES(?);""", value)
             db.conn.commit()
         except db.sqlite3.Error as error:
             print("Something went wrong", error)
+        return 'Note saved'
 
-    def change(self, note: str, id: int):
-        update_note = (note, id)
+    def change(self, arg):
+        fields = list(arg.keys())
+        update_note = (arg[fields[1]], arg[fields[0]])
         try:
-            db.cur.execute("""UPDATE notes
-              SET note = ?
-              WHERE id = ?""", update_note)
+            db.cur.execute(
+                """UPDATE notes
+                SET note = ?
+                WHERE id = ?""", update_note)
             db.conn.commit()
         except db.sqlite3.Error as error:
             print("Something went wrong", error)
+        return 'Note edited'
 
-    def delete(self, id: int):
+    def delete(self, arg):
         try:
             db.cur.execute("""DELETE FROM notes
-              WHERE id = ?""", (id,))
+              WHERE id = ?""", (arg['id'],))
             db.conn.commit()
         except db.sqlite3.Error as error:
             print("Something went wrong", error)
+        return 'Note deleted'
 
-    def filter_for_tags(self, tag: str):
+    def filter_for_tags(self, arg):
+        result = ''
         try:
-            db.cur.execute("""SELECT note FROM notes WHERE tag = ?;""", tag)
-            print(db.cur.fetchall())
+            db.cur.execute(
+                """SELECT note FROM notes WHERE tag = ?;""", (arg['tag'],))
+            for i in db.cur.fetchall():
+                result += str(i[0]) + '\n'
         except db.sqlite3.Error as error:
             print("Something went wrong", error)
+        if result == '':
+            return 'No matches'
+        else:
+            return result
 
-    def add_tag_to_note(self, tag: str, id: int):
-        add_tag = (tag, id)
+    def add_tag_to_note(self, arg):
+        fields = list(arg.keys())
+        add_tag = (arg[fields[1]], arg[fields[0]])
         try:
             db.cur.execute("""UPDATE notes
                 SET tag = ?
@@ -46,11 +60,26 @@ class NoteBook:
             db.conn.commit()
         except db.sqlite3.Error as error:
             print("Something went wrong. Check your id", error)
+        return 'Tag added'
 
     def search(self, key: str):
+        result = ''
         try:
             db.cur.execute(
                 """SELECT * FROM notes WHERE note like ? ;""", ('%'+key+'%'))
+            for i in db.cur.fetchall():
+                result += str(i[0]) + '\n'
+        except db.sqlite3.Error as error:
+            print("Something went wrong", error)
+        if result == '':
+            return 'No matches'
+        else:
+            return result
+
+    def get_table(self, arg):
+        try:
+            db.cur.execute(
+                """SELECT * FROM notes ;""")
             for i in db.cur.fetchall():
                 print(i, end='\n')
         except db.sqlite3.Error as error:
