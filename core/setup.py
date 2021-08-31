ab9ca9e481785e145be7f2e99a574b16347b2f4c
IhorKomkov <85888113+IhorKomkov@users.noreply.github.com>
2021-08-31 18:04:40 +0300

    v_4.0 entry point added

--- core/setup.py
+++ core/setup.py
@@ -12,4 +12,5 @@ routine to our assistant!',
     maintainer='http://140.238.212.157/index.html',
     license='GNU General Public License',
     packages=find_namespace_packages()
+    entry_points = {'personal_assistant_cli': ['personal_assistant_cli = personal_assistant_cli.main:main']}
 )
