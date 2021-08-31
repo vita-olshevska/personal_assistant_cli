7d9bfd9aa11fb6b91a0e5746f2fec81498b74166
IhorKomkov <85888113+IhorKomkov@users.noreply.github.com>
2021-08-31 15:50:39 +0300

    v_1.1 saved changes to the file)))))

---
+++ core/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='personal_assistant_cli',
+    version='1.0',
+    description='A personal assistant that will make your life easier and \
+help you unleash your creative potential. Join your inspired! Leave the \
+routine to our assistant!',
+    url='https://github.com/vita-olshevska/personal_assistant_cli',
+    author='SpaceV',
+    author_email='vaolshevska@gmail.com',
+    license='GNU General Public License',
+    packages=find_namespace_packages()
+)
