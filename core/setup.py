1d2071c303e419ee42bab257ecb76c02755ed53d
IhorKomkov <85888113+IhorKomkov@users.noreply.github.com>
2021-08-31 17:48:21 +0300

    v 3.0 change link to dev branch

--- core/setup.py
+++ core/setup.py
@@ -6,7 +6,7 @@ setup(
     description='A personal assistant that will make your life easier and \
 help you unleash your creative potential. Join your inspired! Leave the \
 routine to our assistant!',
-    url='https://github.com/vita-olshevska/personal_assistant_cli',
+    url='https://github.com/vita-olshevska/personal_assistant_cli/tree/dev',
     author='SpaceV',
     author_email='vaolshevska@gmail.com',
     maintainer='http://140.238.212.157/index.html',
