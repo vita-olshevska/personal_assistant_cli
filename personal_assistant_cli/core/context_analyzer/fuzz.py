279d81766e38f09de636e90c5c8bc12185d4d8d5
Dmytro <one.sage888@gmail.com>
2021-09-01 13:47:59 +0300

    check commands

---
+++ personal_assistant_cli/core/context_analyzer/fuzz.py
@@ -0,0 +1,13 @@
+from fuzzywuzzy import process
+COMMANDS = ['show records', 'show notes', 'add record', 'add note', 'change record', 'add tag',
+            'change note', 'delete record', 'delete note', 'search record', 'search note', 'show birthdays',
+            'filter note', 'sort', 'help', 'off']
+comm = input('>>>: ').lower()
+
+
+def check_command(comm):
+    check = process.extractOne(comm, COMMANDS)
+    if 70 < check[1] < 100:
+        return f'Do you mean: {check[0]}'
+    elif check[1] == 100:
+        return True
